7b5504eb8f9ff169478aeb390dbf769ba0fabc50
Ahsan Ullah Khan <ahsanullahkhan0369@gmail.com>
2023-05-10 12:37:02 +0500

    reading the static data using expressjs

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 
-app.get("/users", (request, respond) => {
+app.use(express.static("public"));
+const __dirname = path.resolve();
+
+const publicPath = path.join(__dirname, "public");
+console.log(publicPath);
+
+app.use((request, respond, next) => {
   console.log(request.url, request.method);
-  //   respond.send("<H1>Hello There!</h1>");
-  respond.json({
+  //   respond.status(503).send("<h1>The site is under maintenance</h1>");
+
+  next();
+});
+app.use(express.static(publicPath)); // this line of code allow us to access to static content(JS,CSS or html) in project from specified folder
+// app.use(express.static(path.join(__dirname, "public")));
+
+// app.get("/maintenance", (request, respond, next) => {
+//   next();
+// });
+const users = [
+  {
     name: "Ahsan Ullah Khan",
-    rollno: 190369,
-    country: {
-      city: "Karachi",
-    },
-  });
+    id: 190369,
+  },
+  {
+    name: "Rabees Waheed",
+    id: 190316,
+  },
+  {
+    name: "Aqeel Mirani",
+    id: 191452,
+  },
+];
+
+app.get("/users", (request, respond) => {
+  respond.status(201).send(users);
+});
+
+app.get("/help", (request, respond) => {
+  const helpFilePath = path.join(__dirname, "public/help.html");
+  respond.sendFile(helpFilePath);
+});
+
+app.get("*", (request, respond) => {
+  const filePath = path.join(__dirname, "public/404.html");
+  respond.sendFile(filePath);
 });
 
 app.listen(3030, () => {
